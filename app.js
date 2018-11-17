3534f4abd70a1ea19967edf4d225c2297fd9be08
prodes13 <ciobotaru.florin@gmail.com>
2018-11-17 22:22:08 +0200

    adding more feature

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 var app = angular.module("HangmanApp", []);
 app.controller("GameController", ['$scope', function($scope) {
-    $scope.demo = "some string";
+
+    var words = ["rat", "dog", "cat", "mat", ];
+    $scope.incorrectLettersChosen = [];
+    $scope.correctLettersChosen = [];
+    $scope.guesses = 6;
+    $scope.displayWord = '';
+    $scope.input = {
+        letter: ''
+    }
+    var selectRandomWord = function() {
+        var index = Math.round(Math.random()*(words.length-1));
+        return words[index];
+    }
+    console.warn(selectRandomWord());
+
+    var newGame = function() {
+        $scope.incorrectLettersChosen = [];
+        $scope.correctLettersChosen = [];
+        $scope.guesses = 6;
+        $scope.displayWord = '';
+
+        selectedWord = selectRandomWord();
+        console.log(selectedWord);
+    }
+
+    newGame();
+    
 }]);
